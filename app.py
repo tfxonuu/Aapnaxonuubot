21462aac41cfa8aa5e3c3f4a1332ac20552d45f0
tfxonuu <iamtfsonu@gmail.com>
2025-10-19 17:16:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,42 +4,41 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return """
-<!DOCTYPE html>
-<html lang="en">
-
-<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <body>
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://github.com/nikhilsainiop" class="card">
-            <p>
-	    <center>
-	        <br
-              /><br
-              />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
-	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
-              />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
-	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
-              />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br 
-	      /><br
-              /><br>
-                <b>Powered By SAINI BOTS</b>
-		</center>
-            </p>
-        </a>
+    <a href="https://github.com/tfxonuu" class="card">
+    <p>
+    <center>
+    <br
+    /><br
+    /
+    /
+    /
+    /
+    /><br
+    /><br>
+    <b>Powered By TFXONU</b>
+    </center>
+    </p>
+    </a>
     </div>
-	<br></br>
-        <center>
-	<footer class="bg-dark text-white text-center py-3 mt-5">
-		<div class="footer__copyright">
-            <p class="footer__copyright-info">
-                © 2025 Video Downloader. All rights reserved.
-            </p>
-        </div>
+    <br></br>
+    <center>
+    <footer class="bg-dark text-white text-center py-3 mt-5">
+    <div class="footer_copyright">
+    <p class="footer_copyright-info">
+    0 2025 Video Downloader. All rights reserved.
+    </p>
+    </div>
     </footer>
     </center>
-</body>
-
-</html>
-"""
+    </body>
+    
+    </html>
+    """
 
 
 if __name__ == "__main__":
